f021ffc72a8a1d78edfc37c3afffaac0fec7003d
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2023-07-11 21:42:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
 // your code here
+	 if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let a = 0;
+    let b = 1;
+    let fib = 0;
+
+    for (let i = 2; i <= num; i++) {
+      fib = a + b;
+      a = b;
+      b = fib;
+    }
+
+    return fib;
+  }
 }
 
 module.exports = fibonacci;
