ea249c73e854b6567a126f442a80aa37a184b8d2
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2024-08-15 18:02:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 function fibonacci(num) {
-// your code here
-	 if (num === 0) {
+  if (num === 1) {
     return 0;
-  } else if (num === 1) {
+  } else if (num === 2) {
     return 1;
   } else {
     let a = 0;
     let b = 1;
     let fib = 0;
 
-    for (let i = 2; i <= num; i++) {
+    for (let i = 3; i <= num; i++) {
       fib = a + b;
       a = b;
       b = fib;
     }
 
-    return fib;
+    return b;
   }
 }
 
